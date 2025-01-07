08796cc4377c3dfe511ff39938e0dba2c8ac07e1
Girish Kumar <girish.kumar3517@email.com>
2025-01-08 00:11:57 +0530

    Checking entered number is Even or Odd

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+# Checking Entered number is Even or Odd.
+
+num = int(input("Enter any number:- "))
+
+if(num % 2 == 0):
+    print("Entered number is Even")
+else:
+    print("Entered number is Odd")
