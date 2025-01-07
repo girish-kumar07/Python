2f8da2891ff329156829f5626376e3b95a14e6a1
Girish Kumar <girish.kumar3517@email.com>
2025-01-08 01:18:32 +0530

    Counting the number of Grade in list

---
+++ list_count.py
@@ -0,0 +1,14 @@
+# Use Capital letter for Grading of students.
+
+grade = []
+
+grade.append(input("Enter the Grade of student1:- "))
+grade.append(input("Enter the Grade of student2:- "))
+grade.append(input("Enter the Grade of student3:- "))
+grade.append(input("Enter the Grade of student4:- "))
+grade.append(input("Enter the Grade of student5:- "))
+grade.append(input("Enter the Grade of student6:- "))
+
+print(grade)
+
+print("Number of Student is:-  ",grade.count(input("Enter the Grade you want to count- ")))
