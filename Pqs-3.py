eec27fe01d805c5767f601827da041e05b1b9c47
Girish Kumar <girish.kumar3517@gmail.com>
2025-06-15 02:25:19 -0700

    Simple Question practice

---
+++ Pqs-3.py
@@ -0,0 +1,6 @@
+num_1 = int(input("Enter the first number:- "))
+num_2 = int(input("Enter the second number:- "))
+if num_1 % num_2 == 0:
+    print("first number is divisible by second number.")
+else:
+    print("first number is not divisible by second number.")
