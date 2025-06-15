605d63d34f02c3f77623cd756e232ccba4d99cbc
Girish Kumar <girish.kumar3517@gmail.com>
2025-06-15 02:24:08 -0700

    Seconds in a year

---
+++ Pqs-2.py
@@ -0,0 +1,7 @@
+seconds_in_a_minute = 60
+minutes_in_an_hour = 60
+hours_in_a_day = 24
+days_in_a_non_leap_year = 365
+
+seconds_in_a_year = seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day * days_in_a_non_leap_year
+print(seconds_in_a_year)
