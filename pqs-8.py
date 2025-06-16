ed85ee30d9b4ca648e52952187c79e67c12b4deb
Girish Kumar <girish.kumar3517@gmail.com>
2025-06-16 12:16:46 -0700

    Miles in to Kilometers

--- pqs-8.py
+++ pqs-8.py
@@ -1,7 +1,7 @@
 miles = int(input("Enter the miles:- "))
 print("Miles\tKilometer")
 print("------------------")
-for i in range(0,miles):
+for i in range(1,miles+1):
     kilometer = i * 1.609
-    i = i+1
+    # i = i+1
     print(i,"\t",kilometer)
