0e764ee4300346be7bb60ea9566d5b9b86d48838
Girish Kumar <girish.kumar3517@gmail.com>
2025-06-19 12:27:48 -0700

    Finding longest word in List

---
+++ pqs-15.py
@@ -0,0 +1,6 @@
+# Finding the longest word in list
+words_list = ['Dictionary','Engineering','Computer','Encyclopedia','History']
+
+longest_word = max(words_list , key=len)
+
+print("The longest word is ",longest_word)
